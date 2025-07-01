cbb05f88f1838cd608d758d4e9a91226792a074b
katsiev1997 <katsiev1997@gmail.com>
2025-07-01 18:42:01 +0300

    05: Auth Socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,16 @@
-"use client";
+import { auth } from "@/lib/auth";
+import { HomeView } from "@/modules/home/ui/views/home-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import React from "react";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { signIn, signOut, signUp, useSession } from "@/lib/auth-client";
-import { useState } from "react";
+export default async function Home() {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
 
-export default function Home() {
-	const { data: session, isPending, error, refetch } = useSession();
-
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-
-	const onSignUp = () => {
-		signUp.email(
-			{
-				email,
-				password,
-				name,
-			},
-			{
-				onError: () => {
-					window.alert("Something went wrong");
-				},
-				onSuccess: () => {
-					window.alert("User created");
-				},
-			}
-		);
-	};
-
-	const onLogin = () => {
-		signIn.email(
-			{
-				email,
-				password,
-			},
-			{
-				onError: () => {
-					window.alert("Something went wrong");
-				},
-				onSuccess: () => {
-					window.alert("User created");
-				},
-			}
-		);
-	};
-
-	if (session) {
-		return (
-			<div className="flex flex-col p-4 gapy-4">
-				<p>Logged in as {session.user.name}</p>
-				<Button onClick={() => signOut()}>Sign out</Button>
-			</div>
-		);
+	if (!session) {
+		redirect("/sign-in");
 	}
-
-	return (
-		<div className="flex flex-col gap-y-10">
-			<div className="p-4 flex flex-col gap-y-4">
-				<Input
-					placeholder="name"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-				/>
-				<Input
-					placeholder="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<Input
-					placeholder="password"
-					type="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<Button onClick={onSignUp}>Create user</Button>
-			</div>
-			<div className="p-4 flex flex-col gap-y-4">
-				<Input
-					placeholder="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<Input
-					placeholder="password"
-					type="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<Button onClick={onLogin}>Login</Button>
-			</div>
-		</div>
-	);
+	return <HomeView />;
 }
--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -18,10 +18,12 @@ import {
 	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { signUp } from "@/lib/auth-client";
+import { signIn, signUp } from "@/lib/auth-client";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const formSchema = z
 	.object({
@@ -58,11 +60,33 @@ export const SignUpView = () => {
 				name: data.name,
 				email: data.email,
 				password: data.password,
+				callbackURL: "/",
 			},
 			{
 				onSuccess: () => {
 					setPending(false);
 					router.push("/");
+				},
+				onError: ({ error }) => {
+					setPending(false);
+					setError(error.message);
+				},
+			}
+		);
+	};
+
+	const onSocial = (provider: "github" | "google") => {
+		setError(null);
+		setPending(true);
+
+		signIn.social(
+			{
+				provider,
+				callbackURL: "/",
+			},
+			{
+				onSuccess: () => {
+					setPending(false);
 				},
 				onError: ({ error }) => {
 					setPending(false);
@@ -181,19 +205,21 @@ export const SignUpView = () => {
 								<div className="grid grid-cols-2 gap-4">
 									<Button
 										disabled={pending}
+										onClick={() => onSocial("google")}
 										variant="outline"
 										type="button"
 										className="w-full"
 									>
-										Google
+										<FaGoogle />
 									</Button>
 									<Button
 										disabled={pending}
+										onClick={() => onSocial("github")}
 										variant="outline"
 										type="button"
 										className="w-full"
 									>
-										GitHub
+										<FaGithub />
 									</Button>
 								</div>
 								<div className="text-center text-sm">
@@ -210,7 +236,7 @@ export const SignUpView = () => {
 					</Form>
 
 					<div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
-						<img src="logo.svg" alt="Image" className="w-[92px] h-[92px]" />
+						<Image src="logo.svg" alt="Image" width={92} height={92} />
 						<p className="text-2xl font-semibold text-white">Meet.AI</p>
 					</div>
 				</CardContent>
